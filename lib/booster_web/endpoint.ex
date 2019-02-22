028c19dee221e28be9218a9bc192216e7d48f730
Alexey Medvedev <medvedev.alexey@gmail.com>
2019-02-22 14:34:52 +0300

    Heroku config

--- lib/booster_web/endpoint.ex
+++ lib/booster_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule ABWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :booster
 
   socket "/socket", ABWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
